5c263550ef1224560ebb5ac9db49b8431e5cc9e5
TanThanh0412 <61406559+TanThanh0412@users.noreply.github.com>
2021-04-13 07:55:09 +0700

    Person-Avatar

---
+++ ChatFeed.jsx
@@ -0,0 +1,19 @@
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+import MessageForm from './MessageForm';
+
+const ChatFeed = (props) => {
+  const { chats, activeChat, userName, messages } = props;
+
+  const chat = chats && chats[activeChat];
+
+  const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+    <div
+      key={`read_${index}`}
+      className="read-receipt"
+      style={{
+        float: isMyMessage ? 'right' : 'left',
+        backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+      }}
+    />
+  ));
