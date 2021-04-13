6ef825a4e66bb9afabc5a90aded96082ed2da1ed
TanThanh0412 <61406559+TanThanh0412@users.noreply.github.com>
2021-04-13 07:56:29 +0700

    update-chatfeed

--- ChatFeed.jsx
+++ ChatFeed.jsx
@@ -17,3 +17,26 @@ const ChatFeed = (props) => {
       }}
     />
   ));
+  
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: '100%' }}>
+          <div className="message-block">
+            {isMyMessage
+              ? <MyMessage message={message} />
+              : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />}
+          </div>
+          <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+            {renderReadReceipts(message, isMyMessage)}
+          </div>
+        </div>
+      );
+    });
+  };
